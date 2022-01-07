9c949b54236eb074ee667f75b5451b4366d5d019
muditmadan03 <97095900+muditmadan03@users.noreply.github.com>
2022-01-07 22:48:06 +0530

    Add files via upload

---
+++ Assignment 1/output.assgn1.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\91842\Desktop\question1.py =================
+1st Question(Find average of three numbers)
+enter number 1=23
+enter number 2=4
+enter number 3=56
+27.666666666666668
+
+================= RESTART: C:\Users\91842\Desktop\question2.py =================
+2nd Question(Compute a person's income tax)
+Enter Gross Income to the nearest penny=20000
+Enter number of dependents=2
+80.0
+
+================= RESTART: C:\Users\91842\Desktop\question3.py =================
+3rd Question(Store a student's data in a list)
+Enter your SID=21105019
+Enter your Name=Mudit
+Enter your Course Name=Electronics and Communication Engineering
+Enter your cgpa=8.00
+Enter your gender F/M/U=M
+[21105019, 'Mudit', 'Electronics and Communication Engineering', 8.0, 'M']
+
+================= RESTART: C:\Users\91842\Desktop\question4.py =================
+4th Question(List of marks for 5 students)
+Enter marks for 1st Student=67
+Enter marks for 2nd Student=78
+Enter marks for 3rd Student=79
+Enter marks for 4th Student=56
+Enter marks for 5th Student 45
+[45.0, 56.0, 67.0, 78.0, 79.0]
+
+================= RESTART: C:\Users\91842\Desktop\question5.py =================
+5th Question
+['RED', 'GREEN', 'WHITE', 'PINK', 'YELLOW']
+['RED', 'GREEN', 'WHITE', 'YELLOW']
+['RED', 'GREEN', 'WHITE', 'Purple', 'YELLOW']
